5d3e2d766decb835a0f9baa57280d76ef0142c7e
Ferdinand Malcher <ferdinand@malcher.media>
2024-11-11 09:17:27 +0100

    Mock für BookStoreService

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardComponent } from './dashboard.component';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
+import { BookStoreService } from '../shared/book-store.service';
+import { of } from 'rxjs';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -21,7 +23,13 @@ describe('DashboardComponent', () => {
       providers: [
         // BRS ersetzen: Immer, wenn jemand BRS anfordert,
         // wird stattdessen der ratingMock ausgeliefert
-        { provide: BookRatingService, useValue: ratingMock }
+        { provide: BookRatingService, useValue: ratingMock },
+        {
+          provide: BookStoreService,
+          useValue: {
+            getAll: () => of([])
+          }
+        }
       ]
     })
     .compileComponents();
